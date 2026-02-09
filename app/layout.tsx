75f175d753c7e78124e345c2d529366fb1b90cda
arvinrajani <arvinrajani71@gmail.com>
2026-02-09 18:02:46 +0500

    Fix: Remove duplicate PageView tracking from base pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '1974490299809444');
-              fbq('track', 'PageView');
             `,
           }}
         />
